30e840b311e24f007f34245c1a2d797a255ca0e7
Vanessa Niculae <vanessa.alexandra.niculae@gmail.com>
2022-04-08 11:01:54 +0300

    Add project files.

---
+++ lab5/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(123, 79);
+            this.progressBar1.Maximum = 10000000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(377, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 0;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(123, 144);
+            this.progressBar2.Maximum = 10000000;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(377, 23);
+            this.progressBar2.Step = 1;
+            this.progressBar2.TabIndex = 1;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(123, 208);
+            this.progressBar3.Maximum = 10000000;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(377, 23);
+            this.progressBar3.Step = 1;
+            this.progressBar3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thread 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Thread 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Thread 3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(561, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Resume";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(561, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Resume";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(561, 144);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Resume";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(668, 208);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Terminate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(668, 144);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Terminate";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(668, 79);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Terminate";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(96, 309);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(120, 116);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(299, 309);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(120, 116);
+            this.richTextBox4.TabIndex = 15;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(506, 309);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(120, 116);
+            this.richTextBox5.TabIndex = 16;
+            this.richTextBox5.Text = "";
+            this.richTextBox5.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(96, 446);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(120, 23);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "Thread 1 Times";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(299, 446);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(120, 23);
+            this.button8.TabIndex = 18;
+            this.button8.Text = "Thread 2 Times";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(506, 446);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(120, 23);
+            this.button9.TabIndex = 19;
+            this.button9.Text = "Thread 3 Times";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(804, 537);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ lab5/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public uint handle1;
+        public uint handle2;
+        public uint handle3;
+
+        WinApiClass.LPTHREAD_START_ROUTINE thread1;
+        WinApiClass.LPTHREAD_START_ROUTINE thread2;
+        WinApiClass.LPTHREAD_START_ROUTINE thread3;
+
+        public uint firstThread(IntPtr a)
+        {
+            GCHandle h = (GCHandle)a;
+            ProgressBar obj = h.Target as ProgressBar;
+
+            for (int i = obj.Minimum; i < obj.Maximum; i++)
+            {
+                obj.PerformStep();
+            }
+            return 0;
+        }
+         
+
+              
+        /*
+        public uint secondThread(IntPtr y)
+        {
+            for (int j = progressBar2.Minimum; j < progressBar2.Maximum; j++)
+            {
+                progressBar2.PerformStep();
+            }
+            return 0;
+        }
+        public uint thirdThread(IntPtr z)
+        {
+            for (int k = progressBar3.Minimum; k < progressBar3.Maximum; k++)
+            {
+                progressBar3.PerformStep();
+            }
+            return 0;
+        }*/
+        public Form1()
+        {
+            InitializeComponent();
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            GCHandle h1 = GCHandle.Alloc(progressBar1);
+            IntPtr objRef1 = GCHandle.ToIntPtr(h1);
+
+            GCHandle h2 = GCHandle.Alloc(progressBar2);
+            IntPtr objRef2 = GCHandle.ToIntPtr(h2);
+
+            GCHandle h3 = GCHandle.Alloc(progressBar3);
+            IntPtr objRef3 = GCHandle.ToIntPtr(h3);
+
+            thread1 = new WinApiClass.LPTHREAD_START_ROUTINE(firstThread);
+            uint id_thread1;
+
+            handle1 = WinApiClass.CreateThread(IntPtr.Zero, 0, thread1, objRef1, (WinApiClass.ThreadState)0x00000004, out id_thread1);
+            thread2 = new WinApiClass.LPTHREAD_START_ROUTINE(firstThread);
+            uint id_thread2;
+
+            handle2 = WinApiClass.CreateThread(IntPtr.Zero, 0, thread2, objRef2, (WinApiClass.ThreadState)0x00000004, out id_thread2);
+            thread3 = new WinApiClass.LPTHREAD_START_ROUTINE(firstThread);
+            uint id_thread3;
+
+            handle3 = WinApiClass.CreateThread(IntPtr.Zero, 0, thread3, objRef3, (WinApiClass.ThreadState)0x00000004, out id_thread3);
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(button1.Text == "Suspend")
+            {
+                WinApiClass.SuspendThread((IntPtr)handle1);
+                button1.Text = "Resume";
+            }
+            else if(button1.Text == "Resume")
+            {
+                WinApiClass.ResumeThread((IntPtr)handle1);
+                button1.Text = "Suspend";
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.Text == "Suspend")
+            {
+                WinApiClass.SuspendThread((IntPtr)handle2);
+                button3.Text = "Resume";
+            }
+            else if (button3.Text == "Resume")
+            {
+                WinApiClass.ResumeThread((IntPtr)handle2);
+                button3.Text = "Suspend";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Suspend")
+            {
+                WinApiClass.SuspendThread((IntPtr)handle3);
+                button2.Text = "Resume";
+            }
+            else if (button2.Text == "Resume")
+            {
+                WinApiClass.ResumeThread((IntPtr)handle3);
+                button2.Text = "Suspend";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WinApiClass.TerminateThread(handle1, 0);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            WinApiClass.TerminateThread(handle2, 0);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WinApiClass.TerminateThread(handle3, 0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            
+                WinApiClass.FILETIME CreationTime, ExitTime, KernelTime, UserTime;
+                WinApiClass.GetThreadTimes((IntPtr)handle1, out CreationTime, out ExitTime, out KernelTime, out UserTime);
+                WinApiClass.SYSTEMTIME x, y, z, w;
+                WinApiClass.FileTimeToSystemTime(ref CreationTime, out x);
+                WinApiClass.FileTimeToSystemTime(ref ExitTime, out y);
+                WinApiClass.FileTimeToSystemTime(ref KernelTime, out z);
+                WinApiClass.FileTimeToSystemTime(ref UserTime, out w);
+                richTextBox1.Text = richTextBox1.Text + "Creation time: " + x.Day + "/" + x.Month + "/" + x.Year + " " + x.Hour + ":" + x.Minute + ":" + x.Second + "\n";
+                richTextBox1.Text = richTextBox1.Text + "Exit Time: " + y.Day + "/" + y.Month + "/" + y.Year + " " + y.Hour + ":" + y.Minute + ":" + y.Second + "\n";
+                richTextBox1.Text = richTextBox1.Text + "Kernel Time: " + z.Milliseconds + "\n";
+                richTextBox1.Text = richTextBox1.Text + "User Time: " + w.Milliseconds;
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            
+                WinApiClass.FILETIME CreationTime, ExitTime, KernelTime, UserTime;
+                WinApiClass.GetThreadTimes((IntPtr)handle1, out CreationTime, out ExitTime, out KernelTime, out UserTime);
+                WinApiClass.SYSTEMTIME x, y, z, w;
+                WinApiClass.FileTimeToSystemTime(ref CreationTime, out x);
+                WinApiClass.FileTimeToSystemTime(ref ExitTime, out y);
+                WinApiClass.FileTimeToSystemTime(ref KernelTime, out z);
+                WinApiClass.FileTimeToSystemTime(ref UserTime, out w);
+                richTextBox4.Text = richTextBox4.Text + "Creation time: " + x.Day + "/" + x.Month + "/" + x.Year + " " + x.Hour + ":" + x.Minute + ":" + x.Second + "\n";
+                richTextBox4.Text = richTextBox4.Text + "Exit Time: " + y.Day + "/" + y.Month + "/" + y.Year + " " + y.Hour + ":" + y.Minute + ":" + y.Second + "\n";
+                richTextBox4.Text = richTextBox4.Text + "Kernel Time: " + z.Milliseconds + "\n";
+                richTextBox4.Text = richTextBox4.Text + "User Time: " + w.Milliseconds;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            
+                WinApiClass.FILETIME CreationTime, ExitTime, KernelTime, UserTime;
+                WinApiClass.GetThreadTimes((IntPtr)handle3, out CreationTime, out ExitTime, out KernelTime, out UserTime);
+                WinApiClass.SYSTEMTIME x, y, z, w;
+                WinApiClass.FileTimeToSystemTime(ref CreationTime, out x);
+                WinApiClass.FileTimeToSystemTime(ref ExitTime, out y);
+                WinApiClass.FileTimeToSystemTime(ref KernelTime, out z);
+                WinApiClass.FileTimeToSystemTime(ref UserTime, out w);
+                richTextBox5.Text = richTextBox5.Text + "Creation time: " + x.Day + "/" + x.Month + "/" + x.Year + " " + x.Hour + ":" + x.Minute + ":" + x.Second + "\n";
+                richTextBox5.Text = richTextBox5.Text + "Exit Time: " + y.Day + "/" + y.Month + "/" + y.Year + " " + y.Hour + ":" + y.Minute + ":" + y.Second + "\n";
+                richTextBox5.Text = richTextBox5.Text + "Kernel Time: " + z.Milliseconds + "\n";
+                richTextBox5.Text = richTextBox5.Text + "User Time: " + w.Milliseconds;
+            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
